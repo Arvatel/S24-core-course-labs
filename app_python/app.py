182d340645a12bf3819e16e330416e203569df59
Arvatel <namenori74@yandex.ru>
2024-11-10 13:16:24 +0300

    fixed formatting

--- app_python/app.py
+++ app_python/app.py
@@ -7,20 +7,23 @@ TIMEZONE = "Europe/Moscow"
 
 app = Flask(__name__)
 
+
 def get_time(timezone):
-	return datetime.now(timezone)
+    return datetime.now(timezone)
 
 
 def format_time(time, time_format):
-	return time.strftime(time_format)
+    return time.strftime(time_format)
+
 
 @app.route('/')
 def moscow_time_show():
-	timezone = pytz.timezone(TIMEZONE)
-	time = get_time(timezone)
+    timezone = pytz.timezone(TIMEZONE)
+    time = get_time(timezone)
 
-	time_format = format_time(time, TIME_FORMAT)
-	return render_template('index.html', time=time_format)
+    time_format = format_time(time, TIME_FORMAT)
+    return render_template('index.html', time=time_format)
+
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
